61e637e9492170eda391ed806824bac915e3cc9b
parmeped <parmeggianipedro@gmail.com>
2020-10-20 23:01:00 -0300

    Changed import to avoid case import problems

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,10 @@
-import Head from 'next/head';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-const name="Pedro";
-export const siteTitle = "Next.js Sample Site";
-
+const name = 'Pedro'
+export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
   return (
@@ -20,12 +19,11 @@ export default function Layout({ children, home }) {
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      
       <header className={styles.header}>
         {home ? (
           <>
@@ -59,7 +57,7 @@ export default function Layout({ children, home }) {
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
-            <a>← Back to home!</a>
+            <a>← Back to home</a>
           </Link>
         </div>
       )}
