eddfcd22f5ea344e3ecb1a7f816bb0394d14cc75
parmeped <parmeggianipedro@gmail.com>
2020-10-20 22:48:46 -0300

    Reseted git tracking

--- components/layout.js
+++ components/layout.js
@@ -59,7 +59,7 @@ export default function Layout({ children, home }) {
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
-            <a>← Back to home</a>
+            <a>← Back to home!</a>
           </Link>
         </div>
       )}
